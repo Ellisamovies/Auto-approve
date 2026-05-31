92a47457d690c9b392f1832965976498f9ca75d6
Ellisamovies <121487082+Ellisamovies@users.noreply.github.com>
2026-05-31 13:09:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'TechVJ'
-
+    return "TechVJ"
 
 if __name__ == "__main__":
-    app.run()
-
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080))
+    )
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
